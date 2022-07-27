21bf6b64fcd21f2f8d50d3598647760b0967ef1c
I`m so vagetable <657769152@qq.com>
2022-07-27 17:03:47 +0800

    Update method_channel_{{project}}.dart

--- federated/__brick__/{{project}}/{{project}}_platform_interface/lib/src/method_channel_{{project}}.dart
+++ federated/__brick__/{{project}}/{{project}}_platform_interface/lib/src/method_channel_{{project}}.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/services.dart';
 
-import '../{{project}}_platform_interface.dart';
+import '{{project}}_platform_interface.dart';
 
-class MethodChannel{{project}} extends {{project}}Platform {
+class MethodChannel{{.pascalCase()}} extends {{.pascalCase()}}Platform {
   static const MethodChannel _channel =
       const MethodChannel('{{project}}');
 
