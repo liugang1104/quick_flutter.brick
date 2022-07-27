c542538a5f47ec45ff902ebfe8cd9fd375d73dae
I`m so vagetable <657769152@qq.com>
2022-07-27 17:04:56 +0800

    Create {{project}}_platform_interface.dart

---
+++ federated/__brick__/{{project}}/{{project}}_platform_interface/lib/src/{{project}}_platform_interface.dart
@@ -0,0 +1,22 @@
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+import 'method_channel_{{project}}.dart';
+
+abstract class {{project.pascalCase()}}Platform extends PlatformInterface {
+  {{project.pascalCase()}}Platform() : super(token: _token);
+
+  static final Object _token = Object();
+
+  static {{project.pascalCase()}}Platform _instance = MethodChannel{{.pascalCase()}}();
+
+  static {{project.pascalCase()}}Platform get instance => _instance;
+
+  static set instance({{.pascalCase()}}Platform instance) {
+    PlatformInterface.verifyToken(instance, _token);
+    _instance = instance;
+  }
+
+  Future<String?> get platformVersion async {
+    throw UnimplementedError('platformVersion has not been implemented.');
+  }
+}
